e476f14b3fd7ea3df3e1b62456c95f81b083f516
Gurmehar Singh Bakshi <greaser.bakshi@gmail.com>
2021-01-04 14:35:58 +0530

    Assets

---
+++ lib/widgets/category_item.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class CategoryItem extends StatelessWidget {
+  final String title;
+  final Color color;
+
+  CategoryItem({
+    Key key,
+    @required this.title,
+    @required this.color,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(15),
+      child: Text(title),
+      decoration: BoxDecoration(
+        color: color,
+        gradient: LinearGradient(
+          colors: [
+            color.withOpacity(0.6),
+            color,
+          ],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        ),
+        borderRadius: BorderRadius.circular(12),
+      ),
+    );
+  }
+}
